4eae9d54fdbc4402014a0b5ad3e34026f9c7ca4c
nicolas <nicolas.matteazzi@gmail.com>
2018-08-26 10:50:36 +0200

    API name added

--- car.js
+++ car.js
@@ -8,6 +8,8 @@ var getTaxesSumDlg = require('./dlg/GetTaxesSumDelegate');
 var postTaxDlg = require('./dlg/PostTaxDelegate');
 var putTaxDlg = require('./dlg/PutTaxDelegate');
 
+var apiName = 'car';
+
 var app = express();
 
 app.use(function(req, res, next) {
@@ -18,7 +20,7 @@ app.use(function(req, res, next) {
 });
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {res.send({status: 'running'});});
+app.get('/', function(req, res) {res.send({api: apiName, status: 'running'});});
 app.get('/taxes', function(req, res) {logger.apiCalled('car', '/taxes', 'GET', req.query, req.params, req.body); getTaxesDlg.getTaxes({unpaied: req.query.unpaied}).then(function(result) {res.send(result);});});
 app.get('/taxes/sum', function(req, res) {logger.apiCalled('car', '/taxes/sum', 'GET', req.query, req.params, req.body); getTaxesSumDlg.getTaxesSum({unpaied: req.query.unpaied}).then(function(result) {res.send(result);});});
 app.post('/taxes', function(req, res) {logger.apiCalled('car', '/taxes', 'POST', req.query, req.params, req.body); postTaxDlg.postTax(req.body).then(function(result) {res.send(result);});});
